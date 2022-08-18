058030eb8e919d73e1190684867fa1a8ba0d39f9
kylebaugh <kylebaugh1@gmail.com>
2022-08-18 17:20:20 -0600

    Added two functions to index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log('This is an example of setting up a git repo')
 
 console.log('This is an example of a second commit')
+console.log('This is an example of a second commit')
+console.log('This is an example of a second commit')
